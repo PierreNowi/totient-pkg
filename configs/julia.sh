d58839d798e0b456b34ad3c50a6440cb1fbd421c
David Bindel <bindel@cs.cornell.edu>
2014-01-09 13:27:30 -0500

    Update Julia build script to not use external BLAS/LAPACK.

--- configs/julia.sh
+++ configs/julia.sh
@@ -6,9 +6,6 @@ source $HOME/pkg/configs/helper.sh
 set_stage_dl https://github.com/JuliaLang/julia.git
 
 BUILDOPTS="VERBOSE=1 USE_BLAS64=0 "
-for lib in BLAS LAPACK ; do
-  BUILDOPTS="$BUILDOPTS USE_SYSTEM_$lib=1"
-done
 make $BUILDOPTS PREFIX=$PREFIX/julia install
 
 leave_stage
