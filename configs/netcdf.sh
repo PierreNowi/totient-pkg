a6ee3d91c7cb4bd846059519a700f28e175e0f85
David Bindel <bindel@cs.cornell.edu>
2014-01-09 13:28:24 -0500

    Killed explicit paths in netcdf (they solved something that turned out to be a non-issue).

--- configs/netcdf.sh
+++ configs/netcdf.sh
@@ -4,7 +4,5 @@
 # http://www.unidata.ucar.edu/software/netcdf/
 
 source $HOME/pkg/configs/helper.sh
-LDFLAGS=-L$PREFIX/lib \
-CPPFLAGS=-I$PREFIX/include \
 stage_dl_ac ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.0.tar.gz 
 
