54905b314cb82ef2a149b407854c9e46044295fe
David Bindel <bindel@cs.cornell.edu>
2014-01-09 13:28:01 -0500

    Switch libevent build script to use simplified stage_dl_ac function.

--- configs/libevent.sh
+++ configs/libevent.sh
@@ -3,16 +3,5 @@
 # livevent -- an event notification library
 # http://libevent.org/
 
-DIRNAME=libevent-2.0.21-stable
-TARBALL=$DIRNAME.tar.gz
-URL=https://github.com/downloads/libevent/libevent/$TARBALL
-
 source $HOME/pkg/configs/helper.sh
-set_stage
-wgetl $URL
-tar -xzf $TARBALL
-cd $DIRNAME
-./configure --prefix=$PREFIX 
-make
-make install
-leave_stage
+set_stage_dl_ac https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable
