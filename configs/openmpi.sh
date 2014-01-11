948ea0352d6e592be4194eaaaf1343567aff5f0d
David Bindel <bindel@cs.cornell.edu>
2014-01-10 23:11:20 -0500

    Correction to OpenMPI build script

--- configs/openmpi.sh
+++ configs/openmpi.sh
@@ -3,4 +3,4 @@
 # OpenMPI
 
 source $HOME/pkg/configs/helper.sh
-stage_dl_ac http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.bz2
+stage_dl_ac http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.bz2 --prefix=$PREFIX/$DIRNAME
