cda3be5686573e10f29453cef7853d0e8d426664
David Bindel <bindel@cs.cornell.edu>
2014-01-14 14:43:32 -0500

    Minor cosmetic updates to setup_ipython_htc; added setup_ipython_mpi.

--- script/setup_ipython_htc.sh
+++ script/setup_ipython_htc.sh
@@ -6,28 +6,29 @@ if [ -d "$HOME/.ipython/profile_htc" ]; then
 fi
 
 ipython profile create --parallel --profile=htc
-pushd $HOME/.ipython/profile_htc
 
-echo > batch_submit <<EOF
-universe        = vanilla
-executable      = /share/cs-instructional/cs5220/local/anaconda/bin/ipengine
-transfer_executable = False
-getenv = True
-notification = never
+PDIR=$HOME/.ipython/profile_htc
+IPENGINE=`which ipengine`
+
+cat > $PDIR/batch_submit <<EOF
+universe            = vanilla
+executable          = $IPENGINE
+transfer_executable = false
+getenv              = true
+notification        = never
 arguments       = "--log-to-file '--profile-dir=$HOME/.ipython/profile_htc' '--cluster-id='"
 
 queue {n}
 EOF
 
-echo > ipcluster_config.py <<EOF
+cat > $PDIR/ipcluster_config.py <<EOF
 c = get_config()
 c.IPClusterStart.engine_launcher_class = 'HTCondorEngineSetLauncher'
 c.HTCondorEngineSetLauncher.batch_template_file = u'$HOME/.ipython/profile_htc/batch_submit'
 EOF
 
-echo > ipcontroller_config.py <<EOF
+cat > $PDIR/ipcontroller_config.py <<EOF
 c = get_config()
 c.HubFactory.ip = '*'
 EOF
 
-popd
