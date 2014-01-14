df44b7a85c5a8a0eef1a6e5d76cf9575608dc66e
David Bindel <bindel@cs.cornell.edu>
2014-01-14 14:44:06 -0500

    Added anaconda post-installation script.

---
+++ script/anaconda_post.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Anaconda post-install setup.  This is my recollection of what
+# I did, but has not actually been tested...
+
+APATH=/share/cs-instructional/cs5220/local/anaconda
+export PATH=$APATH/bin:$PATH
+
+# Make sure we can get pip to play nicely with everything
+conda update conda
+conda update distribute
+conda update setuptools
+conda update pip
+
+# Back up the MPI files
+pushd $APATH/bin
+mkdir mpi-bak
+mv mpi* mpi-bak
+popd
