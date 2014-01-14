6dd53586bdfe38e6673f353148a96dd503e75a2e
David Bindel <bindel@cs.cornell.edu>
2014-01-14 11:31:43 -0500

    Made openmpi-1.6.5 path relative to PREFIX for petsc/tau build scripts.

--- configs/petsc.sh
+++ configs/petsc.sh
@@ -8,7 +8,7 @@ source ./helper.sh
 set_stage_dl http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.4.3.tar.gz
 
 ./configure \
-	--with-mpi-dir=/share/cs-instructional/cs5220/local/openmpi-1.6.5/ \
+	--with-mpi-dir=$PREFIX/openmpi-1.6.5/ \
 	--with-shared-libraries \
 	--prefix=$PREFIX/$DIRNAME
 make all test install
