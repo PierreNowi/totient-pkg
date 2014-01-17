cce9351e92f43345d21875c729089b490883a508
David Bindel <bindel@cs.cornell.edu>
2014-01-17 16:35:26 -0500

    Updated setup.sh to write local matplotlibrc.

--- script/setup.sh
+++ script/setup.sh
@@ -1,13 +1,23 @@
-cat >> .bashrc <<EOF
+cat >> $HOME/.bashrc <<EOF
 
 # Add CS5220 setup
 source /share/cs-instructional/cs5220/script/vars.sh
 EOF
 
-cat >> .bash_profile <<EOF
+cat >> $HOME/.bash_profile <<EOF
 
 # For CS5220 setup
 if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
+EOF
+
+mkdir -p $HOME/.config/matplotlib/
+cat > .config/matplotlib/matplotlibrc <<EOF
+## matplotlibrc for CS5220
+#
+# Customize so that we default to a non-interactive backend.
+# For more details, see http://matplotlib.org/users/customizing.html
+
+backend : Cairo
 EOF
