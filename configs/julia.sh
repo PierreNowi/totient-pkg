96f8b613ee11d2538971aa567bbcd0699094dbd7
David Bindel <bindel@cs.cornell.edu>
2014-04-10 14:26:27 -0400

    Updates to Julia configs.

--- configs/julia.sh
+++ configs/julia.sh
@@ -6,12 +6,7 @@ source ./helper.sh
 set_stage
 set_stage_dl https://github.com/JuliaLang/julia.git
 
-BUILDOPTS="VERBOSE=1 USE_BLAS64=0 USE_SYSTEM_BLAS=1"
-make $BUILDOPTS PREFIX=$PREFIX/julia install
-
-#for fname in $PREFIX/julia/bin/* ; do
-#    echo $fname
-#    patchelf --set-rpath '$ORIGIN/../lib/julia:$ORIGIN/../lib:/share/cs-instructional/cs5220/local/OpenBLAS/lib:/share/cs-instructional/cs5220/local/gcc-4.8.2/lib64:/share/cs-instructional/cs5220/local/gcc-4.8.2/lib:/share/cs-instructional/cs5220/local/lib64:/share/cs-instructional/cs5220/local/lib' $fname
-#done
+BUILDOPTS="JULIA_CPU_TARGET=core2 VERBOSE=1 USE_BLAS64=0 USE_SYSTEM_BLAS=1"
+make $BUILDOPTS prefix=$PREFIX/julia-2014-04-11 install
 
 leave_stage
