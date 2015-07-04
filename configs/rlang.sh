20bf29451677429bad2970209080cdfef3dc44ed
David Bindel <bindel@cs.cornell.edu>
2015-07-04 17:37:53 -0400

    Added R (from CS 5220).

---
+++ configs/rlang.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# R -- a statistics language
+# http://www.r-project.org/
+
+source ./helper.sh
+stage_dl_ac http://lib.stat.cmu.edu/R/CRAN/src/base/R-3/R-3.1.0.tar.gz \
+    --with-lapack="-llapack" \
+    --with-blas="-lopenblas" \
+    --prefix=$PREFIX/R-3.1.0
