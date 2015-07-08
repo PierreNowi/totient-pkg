e34afce9128f922f8f95e02afabe9b268863cf1a
David Bindel <bindel@cs.cornell.edu>
2015-07-08 10:36:16 -0400

    Switched magma-base to be more careful about extra colons in paths.

--- script/magma-base.sh
+++ script/magma-base.sh
@@ -9,17 +9,18 @@ unset module
 
 # Add anaconda base dirs
 
-export PATH=$ANACONDA/bin:$PATH
-export LIBRARY_PATH=$ANACONDA/lib:$LIBRARY_PATH
-export LD_RUN_PATH=$ANACONDA/lib:$LD_RUN_PATH
+export PATH=$ANACONDA/bin${PATH:+":$PATH"}
+export LIBRARY_PATH=$ANACONDA/lib${LIBRARY_PATH:+":$LIBRARY_PATH"}
+export LD_RUN_PATH=$ANACONDA/lib${LD_RUN_PATH:+":$LD_RUN_PATH"}
 
 # Add base directories
 
 export PATH=$MAGMA_BASE/bin:$PATH
-export CPATH=$MAGMA_BASE/include:$CPATH
 export LIBRARY_PATH=$MAGMA_BASE/lib:$MAGMA_BASE/lib64:$LIBRARY_PATH
 export LD_RUN_PATH=$MAGMA_BASE/lib:$MAGMA_BASE/lib64:$LD_RUN_PATH
-export MANPATH=$MAGMA_BASE/man:$MANPATH
+
+export CPATH=$MAGMA_BASE/include${CPATH:+":$CPATH"}
+export MANPATH=$MAGMA_BASE/man${MANPATH:+":$MANPATH"}
 
 export LUA_PATH="$MAGMA_BASE/share/lua/5.3/?.lua;$MAGMA_BASE/share/lua/5.3/?/init.lua"
 export LUA_CPATH="$MAGMA_BASE/lib/lua/5.3/?.so"
