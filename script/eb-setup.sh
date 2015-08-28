a76b2f7ea6d56b39c3e6872ea6baff07043936ae
David Bindel <bindel@cs.cornell.edu>
2015-08-28 16:59:58 -0400

    Killed Condor support script subdir from C4/Magma (irrelevant on totient)

--- script/eb-setup.sh
+++
@@ -1,8 +0,0 @@
-module load EasyBuild
-
-mkdir -p /home/dsb253/.config/easybuild/
-cat > /home/dsb253/.config/easybuild/config.cfg <<EOF
-[config]
-modules-tool=Lmod
-buildpath=/tmp/dsb253/easybuild/build
-EOF
--- script/vars.sh
+++
@@ -1,6 +0,0 @@
-# -- Load default modules
-
-module load gcc-4.9.3
-module load openmpi-1.8.6
-module load openblas
-module load lapack
