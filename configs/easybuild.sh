04c5cac9f25c672f53079947c8c573be8dd56015
David Bindel <bindel@cs.cornell.edu>
2015-08-29 08:21:21 -0400

    Killed packages that I don't plan to install on totient.

--- configs/easybuild.sh
+++
@@ -1,9 +0,0 @@
-source ./helper.sh
-set_stage
-
-EBPATH=$HOME/.local/easybuild
-
-wgetl https://raw.githubusercontent.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py
-python bootstrap_eb.py $EBPATH
-
-leave_stage
