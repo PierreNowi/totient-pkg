a8df5f37ea29074d7b44144f6b97c23aca9e8d3e
David Bindel <bindel@cs.cornell.edu>
2015-08-30 13:14:43 -0400

    Update libunwind to install to utils

--- configs/libunwind.sh
+++ configs/libunwind.sh
@@ -4,4 +4,5 @@
 # http://www.nongnu.org/libunwind/
 
 source ./helper.sh
-stage_dl_ac http://download.savannah.gnu.org/releases/libunwind/libunwind-1.1.tar.gz
+stage_dl_ac http://download.savannah.gnu.org/releases/libunwind/libunwind-1.1.tar.gz \
+	--prefix=$PREFIX/utils
