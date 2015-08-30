170d7e0d758967d75974a257b7ea2440fe05335c
David Bindel <bindel@cs.cornell.edu>
2015-08-30 13:56:23 -0400

    Added up-to-date cmake

---
+++ configs/cmake.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# CMake
+# http://www.cmake.org/
+
+source ./helper.sh
+set_stage_dl http://www.cmake.org/files/v3.3/cmake-3.3.1.tar.gz
+./bootstrap --prefix=$PREFIX/utils
+make
+make install
+leave_stage
