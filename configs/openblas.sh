d35d6886ed5ffa8d0a1c0eebcb28413106d4b632
David Bindel <bindel@cs.cornell.edu>
2015-08-30 14:14:10 -0400

    Update OpenBLAS info.

--- configs/openblas.sh
+++ configs/openblas.sh
@@ -3,14 +3,14 @@
 # OpenBLAS -- Successor to GotoBLAS
 # http://xianyi.github.io/OpenBLAS/
 
-DIRNAME=OpenBLAS
+DIRNAME=OpenBLAS-gcc-5.2.0
 URL=git://github.com/xianyi/OpenBLAS
 
 source ./helper.sh
 set_stage
 git clone $URL
 cd OpenBLAS
-make TARGET=NEHALEM
+make
 make install PREFIX=$PREFIX/$DIRNAME
 rm -f $PREFIX/$DIRNAME/lib/libblas.a
 rm -f $PREFIX/$DIRNAME/lib/libblas.so
