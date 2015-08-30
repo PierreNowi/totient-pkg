c7fb000a81ce05ca794fdb604a10e0e79d918e3e
David Bindel <bindel@cs.cornell.edu>
2015-08-30 12:45:50 -0400

    Added git (and expat prereq) to utils.

---
+++ configs/expat.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# cURL
+# http://expat.sourceforge.net/
+
+source ./helper.sh
+stage_dl_ac http://downloads.sourceforge.net/expat/expat-2.1.0.tar.gz \
+	--prefix=$PREFIX/utils
--- configs/git.sh
+++ configs/git.sh
@@ -5,6 +5,6 @@
 source ./helper.sh
 set_stage_dl https://github.com/git/git/archive/v2.4.5.tar.gz
 
-make prefix=$PREFIX all install
+make prefix=$PREFIX/utils all install
 
 leave_stage
