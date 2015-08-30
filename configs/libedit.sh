1b258ea36ec43127169cc2c7a13df3a7a148349a
David Bindel <bindel@cs.cornell.edu>
2015-08-30 13:14:28 -0400

    Update libedit.sh to install to utils

--- configs/libedit.sh
+++ configs/libedit.sh
@@ -4,5 +4,6 @@
 # http://thrysoee.dk/editline/
 
 source ./helper.sh
-stage_dl_ac http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz
+stage_dl_ac http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz \
+	--prefix=$PREFIX/utils
 
