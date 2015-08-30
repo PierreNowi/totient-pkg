222d505066793e29921579ed09c2e6932e27a468
David Bindel <bindel@cs.cornell.edu>
2015-08-30 13:15:41 -0400

    Update patchelf to install to utils

--- configs/patchelf.sh
+++ configs/patchelf.sh
@@ -3,4 +3,5 @@
 # Patchelf
 
 source ./helper.sh
-stage_dl_ac http://releases.nixos.org/patchelf/patchelf-0.8/patchelf-0.8.tar.bz2
+stage_dl_ac http://releases.nixos.org/patchelf/patchelf-0.8/patchelf-0.8.tar.bz2\
+  --prefix=$PREFIX/utils
