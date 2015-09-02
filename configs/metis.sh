b8924e60f522582be67f1cb7cf61d81ef57a358d
David Bindel <bindel@cs.cornell.edu>
2015-09-02 08:52:26 -0400

    Compile metis 5 into own module

--- configs/metis.sh
+++ configs/metis.sh
@@ -6,7 +6,7 @@
 source ./helper.sh
 set_stage_dl http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz
 
-make config prefix=$PREFIX
+make config prefix=$PREFIX/metis-5.1.0-gcc-4.9.2
 make
 make install
 
