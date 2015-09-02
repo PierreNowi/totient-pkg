86a948136a08e5e1d0a3e474bcc2f07e7a2cb905
David Bindel <bindel@cs.cornell.edu>
2015-09-02 08:54:34 -0400

    Added eigen support

--- configs/eigen.sh
+++ configs/eigen.sh
@@ -3,9 +3,9 @@
 # Eigen -- C++ template library for linear algebra
 # http://eigen.tuxfamily.org/
 
-module load openblas
-module load lapack
-module load suitesparse
+module load openblas/gcc-4.9.2
+module load lapack/3.5.0-gcc-4.9.2
+module load suitesparse/4.4.5-gcc-4.9.2
 
 source ./helper.sh
 set_stage_dl http://bitbucket.org/eigen/eigen/get/3.2.5.tar.bz2
@@ -13,7 +13,7 @@ set_stage_dl http://bitbucket.org/eigen/eigen/get/3.2.5.tar.bz2
 # Build shared libraries
 mkdir build
 cd build
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX/eigen-3.2.5-gcc-4.9.2 \
   -D FFTW_LIBRARIES="-lfftw3" ..
 make
 make install
