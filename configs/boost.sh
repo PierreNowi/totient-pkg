c5d601a41ff6ebc0a5b8b2ef1b6711355387eb48
David Bindel <bindel@cs.cornell.edu>
2015-09-02 08:49:54 -0400

    Load anaconda as basis for Boost::Python

--- configs/boost.sh
+++ configs/boost.sh
@@ -4,9 +4,11 @@
 # http://www.boost.org/
 
 source ./helper.sh
+module load anaconda
+
 set_stage_dl http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.bz2
 
-./bootstrap.sh --prefix=$PREFIX/boost-1.59.0-gcc-5.2.0
+./bootstrap.sh --prefix=$PREFIX/boost-1.59.0-gcc-4.9.2
 ./b2 install
 
 leave_stage
