6e8ebf4dcff5fb2161fe03f18bf31c2e7e32aace
David Bindel <bindel@cs.cornell.edu>
2015-10-01 17:04:21 -0400

    Added IPM to modules.

---
+++ modules/ipm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Generate METIS modules
+#
+cat <<EOF
+#%Module1.0
+proc ModulesHelp { } {
+  global dotversion
+  puts stderr "Integrated Performance Modeling ($TOOLCHAIN build)"
+}
+ 
+module-whatis "Integrated Performance Modeling ($TOOLCHAIN build)"
+set basedir /share/apps/IPM-$TOOLCHAIN
+
+prepend-path PATH \$basedir/bin
+prepend-path LIBRARY_PATH \$basedir/lib
+prepend-path LD_RUN_PATH \$basedir/lib
+EOF
